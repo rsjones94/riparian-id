f9e20db5346a31d526dc1f807e2b598b7e5d0d4a
rsjones94 <rsajones94@gmail.com>
2019-07-22 15:54:42 -0500

    Add soem more raster math

--- trees_from_rasters.py
+++ trees_from_rasters.py
@@ -5,36 +5,48 @@ import os
 import matplotlib.pyplot as plt
 from rasterio.plot import show
 import numpy as np
-plt.ion()
 import matplotlib as mpl
-mpl.rcParams['figure.figsize'] = (14, 14)
-mpl.rcParams['axes.titlesize'] = 20
+
+from osgeo import gdal
 
 parent_folder = r'D:\SkyJones\lidar\2012_tn\system2_overlap\las_products'
 sub_folders = os.listdir(parent_folder)
 
 sub_folders = [sub_folders[2]]
 
+extensions = {'all_intensity': '_allintens.tif',
+              'dem': '_dem.tif',
+              'dem_slope': '_demslope.tif',
+              'dhm': '_dhm.tif',
+              'dsm': '_dsm.tif',
+              'dsm_slope': '_dsmslope.tif',
+              'first_intensity': '_firstintens.tif',
+              'first_intensity_slope': '_firstintensslope.tif',
+              'n_returns': '_nreturns.tif'
+              }
+
+output_folder = 'trees_prediction'
+
 for folder in sub_folders:
-    work_folder = os.path.join(parent_folder, folder, 'working')
+    files = {key: os.path.join(parent_folder, folder, folder+ext) for key, ext in extensions.items()}
+
+    data = {}
+    for key, file in files.items():
+        with rio.open(os.path.join(file), 'r') as src:
+            data[key] = src.read(1)
+            old_meta = src.profile
+
+    work_folder = os.path.join(parent_folder, folder, output_folder)
     os.mkdir(work_folder)
     
-    with rio.open(os.path.join(parent_folder, folder, folder+'_dhm.tif'), 'r') as src:
-        data = src.read(1)
-        old_meta = src.profile
-    mask = data > 3
-    new_data = mask.astype(int)
+    dhm_mask = data['dhm'] >= 3
+    n_returns_mask = data['n_returns'] > 1.5
 
-    write_tf = old_meta["transform"]
-    write_crs = old_meta["crs"]
+    sum_mask = np.logical_and(dhm_mask, n_returns_mask)
+    final_mask = sum_mask.astype(int)
 
-    """
-    fig, ax = plt.subplots(figsize=(12, 6))
-    nd = ax.imshow(new_data)
-    ax.set(title="Filter")
-    ax.set_axis_off()
-    """
+    out_data = sum_mask.astype(int)
 
-    old_meta['dtype'] = str(new_data.dtype)
-    with rio.open(os.path.join(parent_folder, folder, 'working', 'filter.tif'), 'w', **old_meta) as dst:
-        dst.write(new_data, 1)
+    old_meta['dtype'] = str(out_data.dtype)
+    with rio.open(os.path.join(parent_folder, folder, output_folder, 'trees.tif'), 'w', **old_meta) as dst:
+        dst.write(out_data, 1)
