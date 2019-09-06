297fee057e09d7d7ffe0b65f37ce11547dab7f2a
rsjones94 <rsajones94@gmail.com>
2019-09-05 19:55:50 -0500

    no message

--- oneoffs/move_intersecting_las.py
+++ oneoffs/move_intersecting_las.py
@@ -1,8 +1,24 @@
+import os
+
 import whitebox
 wbt = whitebox.WhiteboxTools()
 
-indir = r'D:\SkyJones\gen_veg\sys2_analysis\lidar\2012_tn\system2_overlap\overlap_las'
-outdir = r'D:\SkyJones\gen_model\080102040304\LiDAR\2012\las'
-polygons = r'D:\SkyJones\gen_model\080102040304\study_area\huc_080102040304.shp'
 
-wbt.select_tiles_by_polygon(indir,outdir,polygons)
+parent = r'D:\SkyJones\gen_model\study_areas'
+subs = os.listdir(parent)
+
+for sub in subs:
+    print(f'\n\n!!!!!!!!!!!!!!!\n Working on {sub} \n!!!!!!!!!!!!!!!\n\n')
+    working = os.path.join(parent,sub)
+    study = os.path.join(working,'study_area','study_area_r.shp')
+    lidar_folder = os.path.join(working,'LiDAR')
+
+    possible = os.listdir(lidar_folder)
+    year_folder = [i for i in possible if '20' in i]
+    assert len(year_folder) == 1
+    all_las = os.path.join(lidar_folder,year_folder[0],'las')
+    target = os.path.join(working,'study_LiDAR','las')
+
+    wbt.select_tiles_by_polygon(all_las, target, study)
+
+print('Finished')
