d507a5f4f8cba4aeacabde73b803c1de6f7df297
Sky Jones <rsajones94@gmail.com>
2019-11-04 22:42:46 -0600

    no message

--- write_prediction_rasters.py
+++ write_prediction_rasters.py
@@ -8,7 +8,7 @@ from joblib import dump, load
 from generate_full_predictions import predict_cover
 
 hucs = ['080102040304']
-model = 'tn'
+models = ['tn_full_training_set', 'tn_limited_training_set']
 
 ###
 par = r'F:\gen_model'
@@ -16,8 +16,9 @@ par = r'F:\gen_model'
 sas = pd.read_excel(os.path.join(par, r'study_areas.xlsx'), dtype={'HUC12': object})
 sas = sas.set_index('HUC12')
 
-for huc in hucs:
-    fol = os.path.join(par, 'study_areas', huc)
-    model_folder = os.path.join(par, 'models', model)
-    clf, feature_cols = load(os.path.join(model_folder, 'clf_package.joblib'))
-    img = predict_cover(fol, os.path.join(model_folder,huc), feature_cols, clf, sas.loc[huc].EPSG)
+for model in models:
+    for huc in hucs:
+        fol = os.path.join(par, 'study_areas', huc)
+        model_folder = os.path.join(par, 'models', model)
+        clf, feature_cols = load(os.path.join(model_folder, 'clf_package.joblib'))
+        img = predict_cover(fol, os.path.join(model_folder,huc), feature_cols, clf, sas.loc[huc].EPSG)
