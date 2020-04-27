cc6b0da7b0fa465bf1abd1ec12b970bc79d946f8
Sky Jones <rsajones94@gmail.com>
2020-04-27 01:06:14 -0500

    no message

--- unsupervised_exploration.py
+++ unsupervised_exploration.py
@@ -27,7 +27,8 @@ import matplotlib
 from misc_tools import invert_dict, prune_duplicate_leaves
 from generate_full_predictions import create_predictions_report
 
-matplotlib.use('Agg')
+#matplotlib.use('Agg')
+matplotlib.use('TkAgg')
 
 par = r'F:\gen_model'
 training_folder = r'F:\gen_model\training_sets'
@@ -35,12 +36,26 @@ models_folder = r'F:\gen_model\models'
 unsup_folder = r'F:\gen_model\exploratory\unsupervised'
 n_rand = None  # number of samples from each table. None for all samples
 ignore_cols = ['cellno', 'classification', 'huc12', 'weight']
-var_required = 0.8
+code_file = os.path.join(training_folder, 'class_codes.xlsx')
+var_required = 0.9
+keep_frac = 0.5
+epsilon = 0.15
+#num = int(1e5)
+num = None
 
-training_hucs = ['130202090102']  # if None train all
+training_hucs = ['080102040304']  # if None train all
+# training_hucs = None
 model_name = 'testing'
 
 ####
+
+codes = pd.read_excel(code_file)
+code_dict = {code: num for code, num in zip(codes['t_code'], codes['n_code'])}
+inv_code_dict = {num: code for code, num in zip(codes['t_code'], codes['n_code'])}
+inv_code_dict_full = {num: code for code, num in zip(codes['category'], codes['n_code'])}
+
+inv_plot_dict_color = {num: clr for clr, num in zip(codes['color'], codes['n_code'])}
+inv_plot_dict_linestyle = {num: ls for ls, num in zip(codes['linestyle'], codes['n_code'])}
 
 base_folder = os.path.join(unsup_folder, model_name)
 
@@ -86,7 +101,11 @@ print(f'Data read. Elapsed time: {round(read_elap / 60, 2)} minutes')
 
 print(f'Reforming training data')
 training_df_list = [read_tables[huc] for huc in training_hucs]
-df = pd.concat(training_df_list, ignore_index=True)
+del read_tables
+del df
+
+df = pd.concat(training_df_list, ignore_index=True, sort=False)
+df = df.sample(n=int(len(df)*keep_frac))
 
 print('Initializing model')
 # https://towardsdatascience.com/machine-learning-clustering-dbscan-determine-the-optimal-value-for-epsilon-eps-python-example-3100091cfbc
@@ -98,12 +117,15 @@ print(f'Transforming data')
 
 x = df.loc[:, keep_cols].values
 y = df.loc[:,['classification']].values
-
+del df
+del training_df_list
 xt = StandardScaler().fit_transform(x)
+del x
 xt = pd.DataFrame(xt)
 
 pca = PCA()
 x_pca = pca.fit_transform(xt)
+del xt
 x_pca = pd.DataFrame(x_pca)
 x_pca.head()
 
@@ -120,7 +142,16 @@ print(explained_variance)
 
 x_pca = x_pca.iloc[:, 0:n]
 x_pca['classification'] = y
+print(f'There are {len(x_pca)} entries')
 
+#num = None
+if num is not None:
+    data = x_pca.sample(n=num)
+else:
+    data = x_pca
+
+del x_pca
+del y
 
 """
 print('Plotting')
@@ -132,17 +163,42 @@ plt.savefig(outname)
 """
 
 """
-print('Clustering')
-dbscan = DBSCAN(eps=1, min_samples=len(df)/100/2)
-dbscan.fit(df)
-"""
 
 print('Finding neighbors')
+#neigh = NearestNeighbors(n_neighbors=2, radius=0.1, leaf_size=500)
 neigh = NearestNeighbors(n_neighbors=2)
-nbrs = neigh.fit(df)
-distances, indices = nbrs.kneighbors(df)
+nbrs = neigh.fit(data.iloc[:, 0:n])
+print('Neighbors found')
+distances, indices = nbrs.kneighbors(data.iloc[:, 0:n])
 
-print('Plotting')
+print('Sorting')
 distances = np.sort(distances, axis=0)
+print('Trimming')
 distances = distances[:,1]
-plt.plot(distances)
+print('Plotting')
+try:
+    plt.plot(distances)
+    plt.show()
+except:
+    plt.plot(distances)
+    plt.show()
+"""
+
+"""
+print('Clustering')
+dbscan = DBSCAN(eps=1, min_samples=int(len(data)/100/2))
+dbscan.fit(data.iloc[:, 0:n])
+
+clusters = dbscan.labels_
+data['cluster'] = clusters
+
+
+colors = ['royalblue', 'maroon', 'forestgreen', 'mediumorchid',
+          'tan', 'deeppink', 'olive', 'goldenrod', 'lightcyan', 'navy']
+vectorizer = np.vectorize(lambda x: colors[x % len(colors)])
+
+
+#plt.scatter(data.iloc[:, 0], data.iloc[:, 1], alpha=0.1, c=vectorizer(clusters))
+"""
+cees = [inv_plot_dict_color[n] if n in [1,2,3,8,14,17] else 'black' for n in data['classification']]
+plt.scatter(data.iloc[:, 0], data.iloc[:, 1], alpha=0.3, c=cees)
